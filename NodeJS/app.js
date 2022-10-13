de0696f9c5e3c53691212a382016357202ccfd98
j1powder <93947216+j1powder@users.noreply.github.com>
2022-10-13 15:47:00 -0700

    Add files via upload

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -1,7 +1,25 @@
-const http = require('http');
-const routes = require('./routes');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded());
+
+app.use('/add-product',(req, res, next) => {
+    console.log('In the middleware!');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+app.post('/product', (req, res, next) => {
+ console.log(req.body);
+    res.redirect('/'); 
+});
+
+app.use('/',(req, res, next) => {
+    console.log('In the middleware!');
+    res.send('<h1>Hello from express</h1>');
+});
 
 
-const server = http.createServer(routes);
 
-server.listen(3000); 
+app.listen(3000); 
